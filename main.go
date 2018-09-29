0a759cdd5d289a185b6818932acb5bba0e9fed40
Ryo Ota <nwtgck@gmail.com>
2018-09-29 13:25:50 +0900

    [refactor] Use two spaces

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
-import "fmt"
-import "github.com/cbroglie/mustache"
+import (
+	"fmt"
+	"github.com/cbroglie/mustache"
+)
 
 func main(){
-	data, _ := mustache.Render("hello {{c}}", map[string]string{"c": "world"})
-	fmt.Println(data)
+  data, _ := mustache.Render("hello {{c}}", map[string]string{"c": "world"})
+  fmt.Println(data)
 }
