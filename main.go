adb86fa77c91d888412608fae9f44ce1f32b7652
Ryo Ota <nwtgck@gmail.com>
2018-09-29 13:30:31 +0900

    [change] Print only file paths

--- main.go
+++ main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"github.com/cbroglie/mustache"
 )
 
+// Root directory path
+// TODO: Hard code
+const DirPath = "."
+
 func main(){
-  data, _ := mustache.Render("hello {{c}}", map[string]string{"c": "world"})
-  fmt.Println(data)
+	// Each file in the root directory
+	// (from: https://flaviocopes.com/go-list-files/)
+  filepath.Walk(DirPath, func(path string, info os.FileInfo, err error) error {
+  	// If path is not directory path
+  	if !info.IsDir(){
+  		// Just print file path
+  		fmt.Println(path)
+  	}
+  	return nil
+	})
+	data, _ := mustache.Render("hello {{c}}", map[string]string{"c": "world"})
+	fmt.Println(data)
 }
